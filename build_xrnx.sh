f13c48b47cd167808bf8b0dbbacf08d4b99dd12e
Fay Carsons <faycarsons23@gmail.com>
2024-05-22 13:35:05 -0400

    modify build script to remove old tool

--- build_xrnx.sh
+++ build_xrnx.sh
@@ -6,6 +6,11 @@ tool_dir="lua"
 # Output .xrnx file name
 output_file="ES9.xrnx"
 
+if [ -f ./ES9.xrnx ]; then
+  rm ES9.xrnx
+  echo "Deleted old tool, building new. . ."
+fi
+
 # Create a zip file from the contents of the tool directory and rename it to .xrnx
 zip -r "$output_file" "$tool_dir"
 
